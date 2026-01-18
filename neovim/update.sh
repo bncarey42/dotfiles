7f36ce37923ad0b1cd47713d850d82da3395a116
Ben Carey <bncarey42@users.noreply.github.com>
2026-01-18 00:07:08 -0600

    add update script

---
+++ neovim/update.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+cd ~/Downloads
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x86_64.tar.gz
+sudo rm -rf /opt/nvim-linux-x86_64
+sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
+
+cd -
