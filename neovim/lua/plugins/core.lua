42ab8121c198f56b86c2f6a14bd49dd9a3679f57
Ben Carey <11987913+bncarey42@users.noreply.github.com>
2026-03-26 10:23:18 -0500

    updates for terraform, search, tailwind, and prettier

--- neovim/lua/plugins/core.lua
+++ neovim/lua/plugins/core.lua
@@ -1,62 +1,89 @@
 return {
 
-  -- add pyright to lspconfig
-  {
-    "neovim/nvim-lspconfig",
-    ---@class PluginLspOpts
-    opts = {
-      ---@type lspconfig.options
-      servers = {
-        pyright = {},
-        protols = {},
+   -- add pyright to lspconfig
+   {
+      "neovim/nvim-lspconfig",
+      ---@class PluginLspOpts
+      opts = {
+         ---@type lspconfig.options
+         servers = {
+            pyright = {},
+            protols = {},
+            terraformls = {},
+         },
       },
-    },
-  },
+   },
 
-  -- add more treesitter parsers
-  {
-    "nvim-treesitter/nvim-treesitter",
+   -- add more treesitter parsers
+   {
+      "nvim-treesitter/nvim-treesitter",
 
+      -- Pin to f7955203: commit 8cdffc6d added "tab" to vim query but parser
+      -- doesn't support it yet. See: [github.com/nvim-treesitter/nvim-treesitter](http://github.com/nvim-treesitter/nvim-treesitter)
+      -- Remove this pin once vim parser is updated with :tab support.
 
-    -- Pin to f7955203: commit 8cdffc6d added "tab" to vim query but parser
-    -- doesn't support it yet. See: [github.com/nvim-treesitter/nvim-treesitter](http://github.com/nvim-treesitter/nvim-treesitter)
-    -- Remove this pin once vim parser is updated with :tab support.
-
-    -- commit = "f7955203bb16eed15f9e0fbf7e39b86e0de96b47",
-    lazy = true,
-    opts = {
-      servers = { marksman = {} },
-      ensure_installed = {
-        "bash",
-        "html",
-        "javascript",
-        "json",
-        "lua",
-        "markdown",
-        "markdown_inline",
-        "python",
-        "query",
-        "regex",
-        "tsx",
-        "typescript",
-        "vim",
-        "yaml",
-        "go",
+      -- commit = "f7955203bb16eed15f9e0fbf7e39b86e0de96b47",
+      lazy = true,
+      opts = {
+         servers = { marksman = {} },
+         ensure_installed = {
+            "bash",
+            "html",
+            "javascript",
+            "json",
+            "lua",
+            "markdown",
+            "markdown_inline",
+            "python",
+            "query",
+            "regex",
+            "tsx",
+            "typescript",
+            "vim",
+            "yaml",
+            "go",
+            "terraform",
+            "hcl",
+         },
       },
-    },
-  },
+   },
 
-  -- add any tools you want to have installed below
-  {
-    "mason-org/mason.nvim",
-    opts = {
-      ensure_installed = {
-        "stylua",
-        "shellcheck",
-        "shfmt",
-        "flake8",
-        "protols",
+   -- add any tools you want to have installed below
+   {
+      "mason-org/mason.nvim",
+      opts = {
+         ensure_installed = {
+            "stylua",
+            "shellcheck",
+            "shfmt",
+            "flake8",
+            "prettier",
+            "protols",
+            "tflint",
+         },
       },
-    },
-  },
+   },
+
+   {
+      "mfussenegger/nvim-lint",
+      optional = true,
+      opts = {
+         linters_by_ft = {
+            terraform = { "terraform_validate" },
+            tf = { "terraform_validate" },
+         },
+      },
+   },
+   {
+      "stevearc/conform.nvim",
+      optional = true,
+      opts = {
+         formatters_by_ft = {
+            hcl = { "packer_fmt" },
+            terraform = { "terraform_fmt" },
+            tf = { "terraform_fmt" },
+            ["terraform-vars"] = { "terraform_fmt" },
+         },
+      },
+   },
 }
---
+++ neovim/lua/plugins/terraform.lua
@@ -0,0 +1,20 @@
+return {
+   {
+      "ANGkeith/telescope-terraform-doc.nvim",
+      ft = { "terraform", "hcl" },
+      config = function()
+         LazyVim.on_load("telescope.nvim", function()
+            require("telescope").load_extension("terraform_doc")
+         end)
+      end,
+   },
+   {
+      "cappyzawa/telescope-terraform.nvim",
+      ft = { "terraform", "hcl" },
+      config = function()
+         LazyVim.on_load("telescope.nvim", function()
+            require("telescope").load_extension("terraform")
+         end)
+      end,
+   },
+}
